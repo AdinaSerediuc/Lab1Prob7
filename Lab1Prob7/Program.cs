c5412cae35db0fc12b42d4cbccdff780c4435d90
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-09-19 21:51:54 +0300

    Prob7

--- Lab1Prob7/Program.cs
+++ Lab1Prob7/Program.cs
@@ -8,6 +8,22 @@ namespace Lab1Prob7
     {
         static void Main(string[] args)
         {
+            /*
+             * Ex7
+ Scrieti un program care va verifica daca un numar citit de la tastatura este par sau
+ impar
+  In cazul in care numarul este par, programul va afisa “par” iar in caz contrar, “impar”.
+  Google :even and odd number
+            */
+
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine( x + " este numar par " );
+            }
+            else
+                Console.WriteLine(x + " este numar impar ");
         }
     }
 }
